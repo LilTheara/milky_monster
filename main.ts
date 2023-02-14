35a2a6d04b676912d49dc152512a0d9bf32d5c8f
LilTheara <123979451+LilTheara@users.noreply.github.com>
2023-02-14 14:54:43 -0500

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,16 +2,6 @@ input.onButtonPressed(Button.A, function () {
     pins.servoWritePin(AnalogPin.P0, 180)
     basic.showNumber(180)
 })
-input.onButtonPressed(Button.AB, function () {
-    music.playMelody("D D F - D D F - ", 130)
-    music.playMelody("D D F - D D F - ", 200)
-    music.playMelody("D D F - D D F - ", 250)
-    music.playMelody("D D F - D D F - ", 315)
-    for (let index = 0; index < 3; index++) {
-        music.playMelody("D D F - D D F - ", 400)
-    }
-    music.playMelody("F E G G G - - - ", 400)
-})
 input.onButtonPressed(Button.B, function () {
     pins.servoWritePin(AnalogPin.P0, 180)
     basic.showNumber(0)
